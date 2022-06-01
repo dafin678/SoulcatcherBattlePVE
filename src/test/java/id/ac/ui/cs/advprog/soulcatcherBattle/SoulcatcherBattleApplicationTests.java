505a4faecf2c9502398cdfebc4f795671c070572
muhammad.dhafin01 <muhammad.dhafin01@ui.ac.id>
2022-06-01 22:56:15 +0700

    Initial setup

---
+++ src/test/java/id/ac/ui/cs/advprog/soulcatcherBattle/SoulcatcherBattleApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.soulcatcherBattle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulcatcherBattleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

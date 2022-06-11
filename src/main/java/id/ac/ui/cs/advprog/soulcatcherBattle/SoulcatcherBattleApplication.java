a33bd2b1e1ad2ce051f903b9eac70b3fa1710713
Muhammad Bintang Azriel Aditya Wardhana <74897859+bintangazriel@users.noreply.github.com>
2022-06-11 21:07:14 +0700

    draft reward

--- src/main/java/id/ac/ui/cs/advprog/soulcatcherBattle/SoulcatcherBattleApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/soulcatcherBattle/SoulcatcherBattleApplication.java
@@ -3,6 +3,9 @@ package id.ac.ui.cs.advprog.soulcatcherBattle;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -12,4 +15,9 @@ public class SoulcatcherBattleApplication {
         SpringApplication.run(SoulcatcherBattleApplication.class, args);
     }
 
+    @Bean
+    @LoadBalanced
+    RestTemplate loadBalancedRestTemplate() {
+        return new RestTemplate();
+    }
 }
